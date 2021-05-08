39f5de416f5a44673b888226bb8d6c8c140f68cf
DewanggaDh <73766205+DewanggaDh@users.noreply.github.com>
2021-05-08 21:11:40 +0700

    Update soal3.c

--- soal3/soal3.c
+++ soal3/soal3.c
@@ -25,11 +25,25 @@ void add_file(char filename[], char foldername[])
 
 void* iniinput(void *arg)
 {
+	/*char *argv1[] = {"clear", NULL};
+	pthread_t id = pthread_self();
+	if(pthread_equal(id, tid[0]))
+	{
+		child = fork();
+		if(child == 0)
+		execv("/usr/bin/clear", argv1);
+	}
+	else if(pthread_equal(id, tid[1]))
+	{
+	}*/
 	char directory[PATH_MAX];
-	if(getcwd(directory, sizeof(directory)) != NULL)
-	printf("%s\n", directory); // /home/dewanggad99
+	//getcwd(directory, sizeof(directory));
+	//printf("%s\n", directory); // /home/dewanggad99
+	
 	pthread_t id = pthread_self();
 	char **argv = arg;
+	char extension[PATH_MAX] = {};
+	char source[PATH_MAX] = {}, dest[PATH_MAX] = {}, nope[] = "unknown";
 	if(pthread_equal(id, tid[0]))
 	{
 		int i = 2, j, num = 0;
@@ -37,17 +51,41 @@ void* iniinput(void *arg)
 		while(argv[i] != NULL)
 		{
 			j = 2;
-			char extension[PATH_MAX] = {};
+			getcwd(directory, sizeof(directory));
+			
 			//char extension[PATH_MAX];
-			while(argv[i][j] != '.')
+			while(argv[i][j] != '.' && argv[i][j] != '\0')
 			{
 				j++;
 			}
 			strncat(extension, &garing, 1);
-			strncat(extension, &argv[i][j + 1], PATH_MAX);
+			strncat(extension, &argv[i][0], PATH_MAX);
 			strncat(directory, extension, PATH_MAX);
-			printf("File %d %s\n", i - 1, directory);
+			strncat(source, directory, PATH_MAX);
+			memset(extension, 0, sizeof(extension));
+			memset(directory, 0, sizeof(extension));
+			getcwd(directory, sizeof(directory));
+			
+			strncat(extension, &garing, 1);
+			if(argv[i][j] != '\0')
+			{
+			strncat(extension, &argv[i][j + 1], PATH_MAX);
+			}
+			else
+			{
+			strncat(extension, nope, PATH_MAX);
+			}
+			strncat(extension, &garing, 1);
+			strncat(extension, &argv[i][0], PATH_MAX);
+			strncat(directory, extension, PATH_MAX);
+			strncat(dest, directory, PATH_MAX);
+			printf("%s %s\n", source, dest);
+			rename(source, dest);
 			i++;
+			memset(extension, 0, sizeof(extension));
+			memset(directory, 0, sizeof(directory));
+			memset(dest, 0, sizeof(dest));
+			memset(source, 0, sizeof(source));
 		}
 		
 	}
@@ -108,6 +146,35 @@ int main(int argc, char **argv)
 	pthread_join(tid[0], NULL);
 	pthread_join(tid[1], NULL);
 	pthread_join(tid[2], NULL);
+	//for(i = 0; i < files; i++)
+	//{
+	//	pthread_join(tid[i], NULL);
+	//}
+	//while(i <= files)
+	/*int flag, i = 0;
+	while(i <= argc - 2)
+	{
+		flag = pthread_create(a, NULL, &iniinput, argv);
+	}
+	if(strcmp(argv[1], "-f") == 0)
+	{
+	}
+	else if(strcmp(argv[1], "-d") == 0)
+	{
+	}
+	else if(strcmp(argv[1], "*") == 0)
+	while(i < argc - 2 && strcmp("-f", argv[1]))
+	{
 	
+	if(flag != 0)
+	printf("Nope\n");
+	else
+	printf("Yes\n");
+	}*/
+	/*int flag, i = 0;
+	while(i < 1)
+	{
+		flag = pthread_creat(&(tid[i]), NULL, &iniinput, 
+	}*/
 	return 0;
 }
